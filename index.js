88e63e6e9466f97cc82d982b1f721cacdf421c2c
Julian <julianmartine@hotmail.com>
2022-09-22 16:29:09 -0300

    error

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const {PORT} = require("./src/config")
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(env.PORT, () => {
+  server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
