8516f5eec5c838e9b13789574857da8fe73c04ac
Julian <julianmartine@hotmail.com>
2022-09-22 16:35:06 -0300

    xxx

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {PORT} = require("./src/config")
+const PORT = require("./src/config.js")
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
